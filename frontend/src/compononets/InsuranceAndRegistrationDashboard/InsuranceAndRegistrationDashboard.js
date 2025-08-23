ae0952be000c11ed37d2cb372956c74fa6d30b3c
Lawa-777 <lawanyanisal@gmail.com>
2025-08-23 16:07:09 +0530

    adding Insurance and registration dashboard

---
+++ frontend/src/compononets/InsuranceAndRegistrationDashboard/InsuranceAndRegistrationDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function InsuranceAndRegistrationDashboard() {
+  return (
+    <div>
+      jinki chiki
+      
+    </div>
+  )
+}
+
+export default InsuranceAndRegistrationDashboard
