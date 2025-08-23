db052c048787f5da10b905ecdfb1de9af8291e4c
Ravi-code-2003 <athuraliyaravindu151@gmail.com>
2025-08-23 18:44:34 +0530

    changes

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -38,6 +38,6 @@ mongoose
   )
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    
   })
   .catch((err) => console.error("MongoDB connection error:", err));
